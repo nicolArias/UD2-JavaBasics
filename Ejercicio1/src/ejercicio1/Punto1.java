da13df032cd98b95e988fa373c71787e5d467fa9
Nicol Arias <nicolariaslebro@gmail.com>
2021-02-02 22:19:21 +0100

    Ejercicio 1 de UD2 Java

---
+++ Ejercicio1/src/ejercicio1/Punto1.java
@@ -0,0 +1,42 @@
+package ejercicio1;
+/*Fecha: 02 de Febrero/2021
+ Nicol Dayana Arias Lebro*/
+
+public class Punto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1 - Declara dos variables numéricas (con el valor que desees), muestra por consola la suma,
+		//resta, multiplicación, división y módulo (resto de la división).
+		
+		int num1=5;
+		double num2=12.5;
+		
+		double suma=0;
+		double resta=0;
+		double mult=0;
+		double div=0;
+		double mod=0;
+		
+		suma=num1+num2;
+		System.out.println("Resultado suma: "+suma);
+		
+		resta=num1-num2;
+		System.out.println("Resultado resta: "+resta);
+		
+		mult=num1*num2;
+		System.out.println("Resultado multiplicación: "+mult);
+		
+		div=num1/num2;
+		System.out.println("Resultado división: "+div);
+		
+		mod=num1%num2;
+		System.out.println("Resultado módulo: "+mod);
+		
+		
+		
+		
+
+	}
+
+}
